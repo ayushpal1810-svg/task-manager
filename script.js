aa6b3f266e91a4a466607e83cb47f1f660b78ec2
ayushpal1810-svg <ayushpal1810@gmail.com>
2026-04-21 07:18:14 +0530

    trying to add 'add task feature'

---
+++ script.js
@@ -0,0 +1,7 @@
+const today= new Date();
+const today_formatted_date= today.toLocaleDateString();
+let add;
+document.getElementsByClassName("date")[0].textContent = today_formatted_date;
+document.getElementById("add").addEventListener("click",function () {
+add
+});
